867af7d0ff7474dd9e830fcc08ef0434e6aac3d3
Dexter <dex429.dc@gmail.com>
2020-12-18 19:37:51 -0500

    commit tree 07e7a5d2408a988f67fb0b9c3a71ee79a3dad71b

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,28 @@
-def roll_call_dwarves# code an argument here
+
+
+
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |dwarf, i|
+    puts "#{i + 1}. #{dwarf}"
+  end
+  
+end
+
+def summon_captain_planet(planeteers)
+  planeteers.collect {|planeteer| planeteer = planeteer.capitalize! + '!'}
+end
+
+  # Your code here
+
+def long_planeteer_calls(summon)
+  summon.any?{|summon| summon.length > 4}
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
-
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
+def find_the_cheese(fancy)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  fancy.find do |maybe_fancy| cheese_types.include?(maybe_fancy)
 end
+end
+
